27d348d4a8887bf3afb5627893e51589436d5774
Sangenis Pablo <sangenispablo@gmail.com>
2022-09-28 10:43:02 -0300

    Primer commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post } from "@nestjs/common";
+
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('signin')
+    signin() {
+        return 'Te estas logeando'
+    }
+
+    @Post('signup')
+    signup() {
+        return 'Te estas registrando'
+    }
+
+}
